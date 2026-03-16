3300a2427e10f82e0d2e434751e736f221615042
Quavobliny <kelvinbliny13@gmail.com>
2026-03-16 04:24:49 +0300

    Delete app directory

--- app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Gallus Crew Portal</h1>
-      <p>Machakos University Gallus Crew Management System</p>
-      <p>Owner: Kelvin Muigai Maina</p>
-    </main>
-  );
-}
